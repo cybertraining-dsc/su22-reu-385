5ab427ec91ca57b319fca9e812e783f57d7ef11e
Diyaolu04 <104225033+Diyaolu04@users.noreply.github.com>
2022-07-17 15:06:53 -0500

    time series prediction

--- time-series-prediction/test.py
+++ time-series-prediction/test.py
@@ -13,13 +13,33 @@
 # # # calculate the sample entropy
 # # sampen_of_series = sampen2(series_data)
 
-from sampen import sampen2
+# from sampen import sampen2, normalize_data
+#
+# # initialize a list
+# series_data = []
+#
+# # open the file and read each line into the list
+# with open('sample-entropy/temp3.txt', 'r') as file:
+#     for row in file:
+#         series_data.append(float(row.strip(' \t\n\r')))
+#
+# # calculate the sample entropy
+# print(series_data)
+# sampen_of_series = sampen2(normalize_data(series_data))
+# # print(sampen_of_series)
+# import pandas as pd
+# from statsmodels.tsa.stattools import grangercausalitytests
+# df = pd.read_csv('https://raw.githubusercontent.com/cybertraining-dsc/su22-reu-385/main/time-series-prediction/temp.csv', parse_dates=['date'])
+# df['month'] = df.date.dt.month
+# grangercausalitytests(df[['temperature', 'month']], maxlag=2)
+
+from sampen import sampen2, normalize_data
 
 # initialize a list
 series_data = []
 
 # open the file and read each line into the list
-with open('SampleEntropy/temp3.txt', 'r') as file:
+with open('sample-entropy/temp3.txt', 'r') as file:
     for row in file:
         series_data.append(float(row.strip(' \t\n\r')))
 
